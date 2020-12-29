4baa6b379922ebbaca9fe6cd25e4a083424d538e
Karthik Bhaskar <karthikrajabk@gmail.com>
2020-12-29 11:36:11 -0500

    Problem2

---
+++ s30/PreCourse1/excercise2.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        pass
+
+    def push(self, data):
+        pass
+
+    def pop(self):
+        pass
+
+
+a_stack = Stack()
+while True:
+    print("push <value>")
+    print("pop")
+    print("quit")
+    do = input("What would you like to do? ").split()
+
+    operation = do[0].strip().lower()
+    if operation == "push":
+        a_stack.push(int(do[1]))
+    elif operation == "pop":
+        popped = a_stack.pop()
+        if popped is None:
+            print("Stack is empty.")
+        else:
+            print("Popped value: ", int(popped))
+    elif operation == "quit":
+        break
