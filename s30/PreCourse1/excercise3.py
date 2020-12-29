4fb74c01d0a53d474bbeea2d24cd62dcfcc3a277
Karthik Bhaskar <karthikrajabk@gmail.com>
2020-12-29 14:50:15 -0500

    Solution Updated

--- s30/PreCourse1/excercise3.py
+++ s30/PreCourse1/excercise3.py
@@ -7,7 +7,9 @@ class ListNode:
     """
 
     def __init__(self, data=None, next=None):
-        pass
+
+        self.data = data
+        self.next = next
 
 
 class SinglyLinkedList:
@@ -17,14 +19,21 @@ class SinglyLinkedList:
         Takes O(1) time.
         """
         self.head = None
-        pass
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        pass
+        new_node = ListNode(data)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.next is not None:
+                n = n.next
+            n.next = new_node
 
     def find(self, key):
         """
@@ -32,11 +41,41 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        pass
+        if self.head is None:
+            print("LinkedList is Empty")
+            return
+
+        n = self.head
+        while n is not None:
+            if n.data == key:
+                # print(n.data)
+                return n.data
+            n = n.next
+        return None
 
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
-        pass
+        if self.head is None:
+            print("LinkedList is Empty")
+            return
+
+        # Deleting first node, if key is the first node
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+
+        # Traversing the LinkedList to find the key and delete the key
+        n = self.head
+        while n.next is not None:
+            if n.next.data == key:
+                break
+            n = n.next
+
+        if n.next is None:
+            print("Key not found in the LinkedList")
+
+        else:
+            n.next = n.next.next
