bfdf587ae0898de15099e988b215a1e58326caae
Karthik Bhaskar <karthikrajabk@gmail.com>
2020-12-29 12:53:43 -0500

    Problem3

---
+++ s30/PreCourse1/excercise3.py
@@ -0,0 +1,42 @@
+# Implement Stack Using Singly Linked List
+
+
+class ListNode:
+    """
+    A node in a singly-linked list.
+    """
+
+    def __init__(self, data=None, next=None):
+        pass
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        """
+        Create a new singly-linked list.
+        Takes O(1) time.
+        """
+        self.head = None
+        pass
+
+    def append(self, data):
+        """
+        Insert a new element at the end of the list.
+        Takes O(n) time.
+        """
+        pass
+
+    def find(self, key):
+        """
+        Search for the first element with `data` matching
+        `key`. Return the element or `None` if not found.
+        Takes O(n) time.
+        """
+        pass
+
+    def remove(self, key):
+        """
+        Remove the first occurrence of `key` in the list.
+        Takes O(n) time.
+        """
+        pass
