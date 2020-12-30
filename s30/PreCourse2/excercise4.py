51fe0f997362c19b087002451733d7c1983f9fcb
Karthik Bhaskar <karthikrajabk@gmail.com>
2020-12-30 10:54:22 -0500

    Update Solution

--- s30/PreCourse2/excercise4.py
+++ s30/PreCourse2/excercise4.py
@@ -2,14 +2,55 @@
 def mergeSort(arr):
 
     # write your code here
-    pass
+
+    # Checking wheather the array is empty or not
+    if len(arr) > 1:
+        # Find mid of the array
+        mid = len(arr) // 2
+
+        # Divide the array into two halves L and R
+        L = arr[:mid]
+        R = arr[mid:]
+
+        # Sorting the Left half
+        mergeSort(L)
+        # Sorting the Right half
+        mergeSort(R)
+
+        # Now the actual algorithm starts
+        i = j = k = 0
+
+        # Copy the data to two temp arrays L and R
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i = i + 1
+
+            else:
+                arr[k] = R[j]
+                j = j + 1
+
+            k = k + 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j = j + 1
+            k = k + 1
 
 
 # Code to print the list
 def printList(arr):
 
     # write your code here
-    pass
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
 
 
 # driver code to test the above code
