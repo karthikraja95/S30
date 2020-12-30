6e6d36789a7c3a6a0d20ba61d35662bebab5310e
Karthik Bhaskar <karthikrajabk@gmail.com>
2020-12-30 10:58:58 -0500

    Quick Sort Iterative

---
+++ s30/PreCourse2/excercise5.py
@@ -0,0 +1,11 @@
+# Python program for implementation of Quicksort
+
+# This function is same in both iterative and recursive
+def partition(arr, low, high):
+    # write your code here
+    pass
+
+
+def quickSortIterative(arr, low, high):
+    # write your code here
+    pass
