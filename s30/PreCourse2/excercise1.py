43af57a89698dee1f91095998f37286facc8f6ac
Karthik Bhaskar <karthikrajabk@gmail.com>
2020-12-30 09:33:32 -0500

    Updated Solution

--- s30/PreCourse2/excercise1.py
+++ s30/PreCourse2/excercise1.py
@@ -3,26 +3,18 @@
 
 # It returns location of x in given array arr
 # if present, else returns -1
-def binarySearch(arr, l, r, x):
+def binarySearch(arr, left, right, x):
 
-    """
-    Algorithm:
-    1. Compare x with the middle element.
-    2. If x matches with the middle element, we return the mid index.
-    3. Else if x is greater than the mid element, then x can only lie in the right (greater) half subarray after the mid element.
-        Then we apply the algorithm again for the right half.
-    4. Else if x is smaller, the target x must lie in the left (lower) half. So we apply the algorithm for the left half.
-    """
     # write your code here
     mid = 0
-    while l <= r:
-        mid = (l + r) // 2
+    while left <= right:
+        mid = (left + right) // 2
 
         if arr[mid] < x:
-            l = mid + 1
+            left = mid + 1
 
         elif arr[mid] > x:
-            r = mid - 1
+            right = mid - 1
 
         else:
             return mid
