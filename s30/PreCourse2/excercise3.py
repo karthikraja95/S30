95c1b9e535305c92f122b2b62f824e90c5b007f3
Karthik Bhaskar <karthikrajabk@gmail.com>
2020-12-30 10:08:01 -0500

    Mid point of a linkedlist

---
+++ s30/PreCourse2/excercise3.py
@@ -0,0 +1,29 @@
+# Node class
+class Node:
+
+    # Function to initialise the node object
+    def __init__(self, data):
+        pass
+
+
+class LinkedList:
+    def __init__(self):
+        pass
+
+    def push(self, new_data):
+        pass
+
+    # Function to get the middle of
+    # the linked list
+    def printMiddle(self):
+        pass
+
+
+# Driver code
+list1 = LinkedList()
+list1.push(5)
+list1.push(4)
+list1.push(2)
+list1.push(3)
+list1.push(1)
+list1.printMiddle()
