03ff4b2fa3a9fc4b1ae181fec3fa3e177fe33333
Karthik Bhaskar <karthikrajabk@gmail.com>
2020-12-30 10:23:38 -0500

    Updated Solution

--- s30/PreCourse2/excercise3.py
+++ s30/PreCourse2/excercise3.py
@@ -3,20 +3,47 @@ class Node:
 
     # Function to initialise the node object
     def __init__(self, data):
-        pass
+        self.data = data
+        self.next = None
 
 
 class LinkedList:
+
+    """
+    Algorithm:
+
+    Initialized the temp variable as head
+    Initialized count to Zero
+    Take loop till head will become Null(i.e end of the list) and increment
+    the temp node when count is odd only, in this way temp will traverse
+    till mid element and head will traverse all linked list.
+    Print the data of temp.
+    """
+
     def __init__(self):
-        pass
+        self.head = None
 
     def push(self, new_data):
-        pass
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
 
     # Function to get the middle of
     # the linked list
     def printMiddle(self):
-        pass
+        temp = self.head
+        count = 0
+
+        while self.head:
+            # Only update when count is odd
+
+            if count & 1:
+                temp = temp.next
+            self.head = self.head.next
+
+            count = count + 1
+
+        print(temp.data)
 
 
 # Driver code
