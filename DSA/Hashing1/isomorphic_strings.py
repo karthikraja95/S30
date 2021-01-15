ae7cb2ff2d7a463080b3031b506d159898e6d42d
Karthik Bhaskar <karthikrajabk@gmail.com>
2021-01-15 13:09:48 -0500

    Pythonic Approach using Zip

--- DSA/Hashing1/isomorphic_strings.py
+++ DSA/Hashing1/isomorphic_strings.py
@@ -20,3 +20,33 @@ class Soution:
                 if s[i] not in d or d[s[i]] != t[i]:
                     return False
         return True
+
+
+# More Pythonic Approach:
+"""
+ Time Complexity : O(n) where n is the length of input string
+(as both strings have to be of the same length)
+Space Complexity : O(2n) as we require two DS i.e. HashMap and Set
+but both datastructures can be of max length 26 as it only
+contains all unique letters => O(1)
+"""
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        mapping = {}
+        seen = set()
+
+        for c1, c2 in zip(s, t):
+            if c1 in mapping and mapping[c1] != c2:
+                return False
+            elif c1 not in mapping and c2 in seen:
+                return False
+            else:
+                mapping[c1] = c2
+                seen.add(c2)
+
+        return True
